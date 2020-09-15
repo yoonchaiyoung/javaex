e8366e873b3b103b68af44b95dec16e21d0b4a7e
yoonchaiyoung <domo62@naver.com>
2020-09-15 17:37:24 +0900

    singleton 수정

--- src/com/javaex/oop/staticmember/Singleton.java
+++ src/com/javaex/oop/staticmember/Singleton.java
@@ -3,14 +3,17 @@ package com.javaex.oop.staticmember;
 // 어떤 상황에서든 단 하나의 인스턴스만 유지해야 하는 경우
 public class Singleton {
 	private static Singleton instance = new Singleton();
-	private static int[] scores;
-	static {
-		scores = new int[10];
-	}
+//	private static int[] scores;
+	private int[] scores;
+	
+//	static {
+//		scores = new int[10];
+//	}
 	
 	// 생성자 -> new를 막아준다
 	private Singleton() {
 		// new 호출 불가
+		scores = new int[10];
 		
 	}
 	
