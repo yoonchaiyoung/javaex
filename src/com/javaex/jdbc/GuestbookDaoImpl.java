00303847f5e8e4d45d252cf5af9c5a6f57b2b65f
yoonchaiyoung <domo62@naver.com>
2020-09-21 15:42:45 +0900

    JDBC DAO 패턴

--- src/com/javaex/jdbc/GuestbookDaoImpl.java
+++ src/com/javaex/jdbc/GuestbookDaoImpl.java
@@ -2,6 +2,7 @@ package com.javaex.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -13,7 +14,7 @@ public class GuestbookDaoImpl implements GuestbookDao {
 	String dburl = "jdbc:mariadb://192.168.1.134:3306/mysite?useSSL=false";
 	String dbuser = "mysite";
 	String dbpass = "mysite";
-
+	
 	//	공통 메서드 : 접속
 	private Connection getConnection() throws SQLException {
 		Connection conn = null;
@@ -25,7 +26,7 @@ public class GuestbookDaoImpl implements GuestbookDao {
 		} catch (ClassNotFoundException e) {
 			System.err.println("접속 실패");
 		}
-
+		
 		return conn;
 	}
 	@Override
@@ -34,17 +35,17 @@ public class GuestbookDaoImpl implements GuestbookDao {
 		Connection conn = null;
 		Statement stmt = null;
 		ResultSet rs = null;
-
+		
 		List<GuestbookVo> list = new ArrayList<>();
-
+		
 		String sql = "SELECT no, name, password, message, reg_date " +
 				"FROM guestbook ORDER BY reg_date DESC";
-
+		
 		try {
 			conn = getConnection();
 			stmt = conn.createStatement();
 			rs = stmt.executeQuery(sql);
-
+			
 			//	커서 루프
 			while(rs.next()) {
 				Long no = rs.getLong(1);
@@ -52,7 +53,7 @@ public class GuestbookDaoImpl implements GuestbookDao {
 				String password = rs.getString(3);
 				String message = rs.getString(4);
 				Date regDate = rs.getDate(5);
-
+				
 				//	VO 객체 생성
 				GuestbookVo vo = new GuestbookVo(no, name, password, message, regDate);
 				//	반환할 List에 vo 추가
@@ -70,14 +71,40 @@ public class GuestbookDaoImpl implements GuestbookDao {
 				e.printStackTrace();
 			}
 		}
-
+		
 		return list;	//	list 객체 반환(ResultSet -> List로 변환)
 	}
 
 	@Override
 	public int insert(GuestbookVo vo) {
-		// TODO Auto-generated method stub
-		return 0;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		int insertedCount = 0;	//	INSERT, UPDATE, DELETE 쿼리는 영향을 받은 레코드의 카운트 리턴
+		
+		try {
+			conn = getConnection();
+			String sql = "INSERT INTO guestbook (name, password, message, reg_date) " +
+				" VALUES(?, ?, ?, now())";	//	SQL 실행 계획
+			pstmt = conn.prepareStatement(sql);
+			
+			//	동적 데이터 연결
+			pstmt.setString(1, vo.getName());
+			pstmt.setString(2, vo.getPassword());
+			pstmt.setString(3, vo.getMessage());
+			
+			//	실행 : INSERT, UPDATE, DELETE -> exeucteUpdate()
+			insertedCount = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				pstmt.close();
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return insertedCount;
 	}
 
 	@Override
@@ -94,8 +121,46 @@ public class GuestbookDaoImpl implements GuestbookDao {
 
 	@Override
 	public List<GuestbookVo> searchByKeyword(String keyword) {
-		// TODO Auto-generated method stub
-		return null;
+		List<GuestbookVo> list = new ArrayList<>();
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		String sql = "SELECT no, name, password, message, reg_date FROM guestbook " +
+				"WHERE name LIKE ? OR message LIKE ?";
+		
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, "%" + keyword + "%");
+			pstmt.setString(2, "%" + keyword + "%");
+			
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				Long no = rs.getLong(1);
+				String name = rs.getString(2);
+				String password = rs.getString(3);
+				String message = rs.getString(4);
+				Date regDate = rs.getDate(5);
+				
+				GuestbookVo vo = new GuestbookVo(no, name, password, message, regDate);
+				list.add(vo);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				rs.close();
+				pstmt.close();
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return list;
 	}
 
 }
--- src/com/javaex/jdbc/JDBCEx.java
+++ src/com/javaex/jdbc/JDBCEx.java
@@ -19,17 +19,8 @@ public class JDBCEx {
 		guestbookSearch();
 	}
 	
-	private static void guestbookUpdate() {
-		// 연습문제: 방명록 번호, 비밀번호, 내용을 입력받아
-		// Update하기
-	}
-	
-	private static void guestbookDelete() {
-		// 연습문제: 방명록 번호, 비밀번호 입력받아
-		// Delete하기
-	}
-	
 	private static void guestbookSearch() {
+		//	키워드 입력 받아서 검색 수행
 		guestbookSelectAll();
 		GuestbookDao dao = new GuestbookDaoImpl();
 		
@@ -48,7 +39,7 @@ public class JDBCEx {
 	}
 	
 	private static void guestbookInsert() {
-		// 스캐너로부터 입력 받아서 guestbook 테이블에 등록
+		//	스캐너로부터 입력 받아서 guestbook 테이블에 등록
 		guestbookSelectAll();
 		GuestbookDao dao = new GuestbookDaoImpl();
 		
@@ -60,19 +51,21 @@ public class JDBCEx {
 		System.out.print("메시지(단어):");
 		String message = scanner.next();
 		
-		// DTO 객체 생성
-		GuestbookVo vo = new GuestbookVo(null,	// no 필드 -> PK
+		//	DTO 객체 생성
+		GuestbookVo vo = new GuestbookVo(null,	//	no 필드 -> PK
 				name, password, message,
-				null);	// reg_date 필드 -> now() 세팅
+				null);	//	reg_date 필드 -> now() 세팅
 		int insertedCount = dao.insert(vo);
-		System.out.println(insertedCount + "개의 레코드가 추가되었ㅅ브니다.");
+		
+		System.out.println(insertedCount + "개의 레코드가 추가되었습니다.");
 		
 		guestbookSelectAll();
 		scanner.close();
 	}
+	
 	private static void guestbookSelectAll() {
-		// DAO 생성
-		// 비즈니스 로직과 저장 로직을 분리
+		//	DAO 생성
+		//	비즈니스 로직과 저장 로직을 분리
 		GuestbookDao dao = new GuestbookDaoImpl();
 		List<GuestbookVo> list = dao.searchAll();
 		
@@ -83,44 +76,45 @@ public class JDBCEx {
 		System.out.println("==========");
 	}
 	
-	private static void selectTest() {		
-		// employees 데이터베이스의 employees 테이블로부터 모든 레코드를 받아서 출력
-		String dburl = "jdbc:mariadb://192.168.1.138:3306/employees?useSSL=false";
+	private static void selectTest() {
+		//	employees 데이터베이스의 employees 테이블로부터 모든 레코드를 받아서 출력
+		String dburl = "jdbc:mariadb://192.168.1.134:3306/employees?useSSL=false";
 		String dbuser = "employees";
 		String dbpass = "employees";
 		
-		Connection conn = null;	// java.sql.Connection
-		Statement stmt = null;	// java.sql.Statement
-		ResultSet rs = null;	// java.sql.ResultSet
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
 		
 		try {
-			// 1. 드라이버 로드
+			//	1. 드라이버 로드
 			Class.forName("org.mariadb.jdbc.Driver");
-			// 2. Connection
+			//	2. Connection
 			conn = DriverManager.getConnection(dburl, dbuser, dbpass);
-			// 3. Statement
+			//	3. Statement
 			String sql = "SELECT emp_no, first_name, last_name, birth_date FROM employees";
-			stmt = conn.createStatement();	// Statement 생성
-			// 4. 쿼리 수행
+			stmt = conn.createStatement(); // Statement 생성
+			//	4. 쿼리 수행
 			rs = stmt.executeQuery(sql);
-			// 5. 결과 처리
-			while(rs.next()) {	// 커서에서 다음 레코드를 받아오기
-				// 컬럼 데이터 받아오기
-				Long no = rs.getLong(1);	// 숫자 인덱스 사용 (1부터 시작)
-				String firstName = rs.getString("first_name");	// 문자 인덱스 (컬럼명)
+			//	5. 결과 처리
+			while(rs.next()) {	//	커서에서 다음 레코드를 받아오기
+				//	컬럼 데이터 받아오기
+				Long no = rs.getLong(1);	//	숫자 인덱스 사용 (1부터)
+				String firstName = rs.getString("first_name");	//	문자 인덱스(컬럼명)
 				String lastName = rs.getNString("last_name");
-				Date birthDate = rs.getDate(4);	// java.util.Date
+				Date birthDate = rs.getDate(4);	//	java.util.Date
 				
 				SimpleDateFormat sdf = new SimpleDateFormat("YYYY-mm-dd");
 				System.out.printf("%d - %s %s (생일: %s)%n", 
-						no, firstName, lastName, sdf.format(birthDate));				
+						no, firstName, lastName, sdf.format(birthDate));
 			}
+
 		} catch (ClassNotFoundException e) {
 			System.err.println("드라이버 로드 실패!");
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
-			// 6. 자원 정리
+			//	6. 자원 정리
 			try {
 				rs.close();
 				stmt.close();
@@ -130,4 +124,5 @@ public class JDBCEx {
 			}
 		}
 	}
+
 }
